8d988bac5b9ebc2bc62c69426e1327d8d991390d
gromejpeg <gabeapplemail@gmail.com>
2025-11-24 12:46:46 -0500

    Initial Commit

---
+++ SmartConverter/SmartConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartConverterApp.swift
+//  SmartConverter
+//
+//  Created by Gabriel Romero on 11/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
